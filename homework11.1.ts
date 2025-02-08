52d394d0e9e26318dda7b2211ecd34797770d21f
Rustem Samoilenko <samoilenkorustem@gmail.com>
2025-02-08 22:52:42 +0200

    some fixes

--- homework11.1.ts
+++ homework11.1.ts
@@ -1,28 +1,37 @@
 type DeepMutable<Type> = {
-    -readonly [Property in keyof Type]: Type[Property]
+    -readonly [Property in keyof Type]: Type[Property] extends object
+            ? DeepMutable<Type[Property]> 
+            : DeepMutable<Type[Property]>
 }
 
-type TValueType = 'string' | 'boolean' | 'number' | 'bigint' | 'symbol' | 'undefined' | 'object' | 'function' | 'never'
 
 type PickByValue<Type, ValueType> = {
     [Key in keyof Type]: Type[Key] extends ValueType ? Key : never
-}
-
-type TPickByValue<ValueType extends TValueType> = 
-    ValueType extends 'string' ? string :
-    ValueType extends 'boolean' ? boolean :
-    ValueType extends 'number' | 'bigint' ? number :
-    ValueType extends'symbol'? symbol :
-    ValueType extends 'undefined'? undefined :
-    ValueType extends 'object'? object : 
-    ValueType extends 'function'? Function : never
-
+}[keyof Type]
 
 type OmitByValue<Type, ValueType> = {
     [Key in keyof Type]: Type[Key] extends ValueType ? never : Key
-}
+}[keyof Type]
 
 
 type CustomReturnType<Type> = Type extends (param: (infer U)[]) => void ? U : undefined
 
 type ExtendedCustomReturnType<Type> = Type extends (...args: infer ParameterType) => infer ReturnType ? [ReturnType, ParameterType] : undefined
+
+
+
+interface Personn {
+    name: string;
+    age: number;
+    address: {
+        city: string;
+        country: string;
+    };
+    isActive: boolean;
+}
+
+
+type StringKeys = PickByValue<Personn, string>
+
+
+type OmitStringKeys = OmitByValue<Personn, object>
